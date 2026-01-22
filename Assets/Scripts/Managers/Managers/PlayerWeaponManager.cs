d3390b6196e73623a087589e5627a3b7ead13bfb
Ashley N <ewannicholson2004@gmail.com>
2026-01-22 17:54:43 +0000

    Added secondary HUD icon for equipped weapon

--- Assets/Scripts/Managers/Managers/PlayerWeaponManager.cs
+++ Assets/Scripts/Managers/Managers/PlayerWeaponManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,6 +20,7 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
 
     [Header("Input")]
     public KeyCode GrappleKey = KeyCode.LeftShift;
+    public KeyCode ParryKey = KeyCode.F;
 
     [Header("UI")]
     public Sprite GrappleIcon;
@@ -31,6 +34,11 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
 
     private float lastScrollSwitchTime = -10f;
 
+    [Header("Weapon Bools")]
+    public bool GrappleEnabled = true;
+    public bool ShotgunEnabled = true;
+    public bool RevolverEnabled = true;
+
     private void Awake()
     {
         SaveManager.Instance.Register("Weapons", this);
@@ -39,6 +47,7 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
     private void Start()
     {
         UpdateHUDIcon();
+        UpdateSecondaryHUDIcon();
     }
 
     private void Update()
@@ -53,6 +62,8 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
 
     private void HandleGrappleInput()
     {
+        if (!GrappleEnabled) { return; }
+
         if (Input.GetKeyDown(GrappleKey))
         {
             if (EquippedWeapon != PlayerWeapon.Grapple)
@@ -79,11 +90,11 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
 
             lastScrollSwitchTime = Time.time;
 
-            if (scroll > 0f)
+            if (EquippedWeapon == PlayerWeapon.Revolver && ShotgunEnabled)
             {
                 SetWeapon(PlayerWeapon.Shotgun);
             }
-            else if (scroll < 0f)
+            else if (EquippedWeapon == PlayerWeapon.Shotgun && RevolverEnabled)
             {
                 SetWeapon(PlayerWeapon.Revolver);
             }
@@ -92,12 +103,14 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
         if (scroll == 0f) return;
         if (EquippedWeapon == PlayerWeapon.Grapple) return;
 
-        PlayerWeapon newWeapon =
-            EquippedWeapon == PlayerWeapon.Revolver
-                ? PlayerWeapon.Shotgun
-                : PlayerWeapon.Revolver;
+    }
 
-        SetWeapon(newWeapon);
+    private void HandleParryInput()
+    {
+        if (Input.GetKeyDown(ParryKey))
+        {
+            //  Tell PlayerCombat to parry
+        }
     }
 
     // --------------------
@@ -112,6 +125,7 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
             lastRegularWeapon = weapon;
 
         UpdateHUDIcon();
+        UpdateSecondaryHUDIcon();
     }
 
     private void UpdateHUDIcon()
@@ -134,6 +148,41 @@ public class PlayerWeaponManager : MonoBehaviour, ISaveable
                 break;
         }
     }
+
+    private void UpdateSecondaryHUDIcon()
+    {
+        if (hudController == null || hudController.SecondaryEquipmentIcon == null)
+            return;
+
+        if (EquippedWeapon == PlayerWeapon.Grapple)
+        {
+            if (lastRegularWeapon == PlayerWeapon.Revolver)
+            {
+                //hudController.SecondaryEquipmentIcon.color.a = 1f;// Needs to set alpha to 1
+                hudController.SecondaryEquipmentIcon.sprite = RevolverIcon;
+            }
+            else if (lastRegularWeapon == PlayerWeapon.Shotgun)
+            {
+                //hudController.SecondaryEquipmentIcon.color.a = 1f;
+                hudController.SecondaryEquipmentIcon.sprite = ShotgunIcon;
+            }
+        }   
+        else
+        {
+            if (GrappleEnabled)
+            {
+                //hudController.SecondaryEquipmentIcon.color.a = 1f;
+                hudController.SecondaryEquipmentIcon.sprite = GrappleIcon;
+            }
+            else
+            {
+                //hudController.SecondaryEquipmentIcon.color.a = 0f;// Needs to set alpha to 0
+            }
+        }
+    }
+
+    // Animate weapon that is being unequipped rotating down (shotgun, revolver)
+    // animate weapon that is being equipped going up (shotgun, revolver)
 
     // --------------------
     // Saving
